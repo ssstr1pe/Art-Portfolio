d701aefb6690cf2aefeb97c48ec06d9f142ea2c9
ssstr1pe <str1pe.c0re.1@gmail.com>
2025-12-12 23:51:48 +0000

    fix navbar for mobile

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -10,8 +10,8 @@ const Navbar = () => {
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
           </div>
           <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-            <li><Link href="/">WORK</Link></li>
-            <li><Link href="/Bios">BIO</Link></li>
+            <li><Link href="/">HOME</Link></li>
+            <li><Link href="/Work">WORK</Link></li>
             <li><Link href="/Press">PRESS</Link></li>
             <li><Link href="/Info">INFO</Link></li>
           </ul>
