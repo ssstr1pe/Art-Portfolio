b1f29142be13b5113ee54ea3af12a06430535614
ssstr1pe <str1pe.c0re.1@gmail.com>
2026-02-14 11:35:04 +0000

    fix image quality again

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -10,14 +10,14 @@ const ImagePreview = ({ imagePath }: { imagePath: string }) => {
 
   return (
     <div>
-      <Image src={imagePath} onClick={toggleModal} className="cursor-pointer" alt={""} width={720} height={720} />
+      <Image src={imagePath} onClick={toggleModal} className="cursor-pointer" alt={""} width={720} height={720} quality={100} unoptimized={true} />
       <Modal
         isOpen={showModal}
         onRequestClose={toggleModal}
         className="mx-auto my-10 w-80 md:w-fit bg-white p-2"
         overlayClassName="fixed inset-0 bg-black bg-opacity-50"
       >
-        <Image src={imagePath} alt="Full Size Image" className="w-full h-auto" width={720} height={720} quality={100} />
+        <Image src={imagePath} alt="Full Size Image" width={720} height={720} quality={100} unoptimized={true} />
       </Modal>
     </div>
   );
